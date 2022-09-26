2f8a0ddd26a296a1a116c5f6b8e87702b1c5b36b
pupil <pupil@ivkhk.ee>
2022-09-26 14:34:07 +0300

    Done

--- src/jptv21task2romankukalev/JPTV21Task2RomanKukalev.java
+++ src/jptv21task2romankukalev/JPTV21Task2RomanKukalev.java
@@ -4,7 +4,7 @@
  * and open the template in the editor.
  */
 package jptv21task2romankukalev;
-
+import java.util.Scanner;
 /**
  *
  * @author pupil
@@ -15,7 +15,20 @@ public class JPTV21Task2RomanKukalev {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner input = new Scanner(System.in);
+        System.out.print("Введите трёхзначное число: ");
+        int n = input.nextInt();
+        System.out.println("Сумма цифр равна: " + sumDigits(n));
+            // TODO code application logic here
+        }
+    
+    public static int sumDigits(int n) {
+        int sum = 0;
+        while (n != 0) {
+            sum += n % 10;
+            n /= 10;
+        }
+        return sum;
     }
     
 }
